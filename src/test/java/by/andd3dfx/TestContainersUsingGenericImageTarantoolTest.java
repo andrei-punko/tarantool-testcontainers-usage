ca8d5cd9af3a7ca83d147f63a162d00b27525c2c
Andrei Punko <andd3dfx@gmail.com>
2021-06-09 13:21:35 +0300

    Add example of how to use Tarantool with GenericContainer
    (to avoid Docker volumes mounting)

---
+++ src/test/java/by/andd3dfx/TestContainersUsingGenericImageTarantoolTest.java
@@ -0,0 +1,13 @@
+package by.andd3dfx;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class TestContainersUsingGenericImageTarantoolTest extends BaseTarantoolTest {
+
+    @Test
+    void test() {
+
+    }
+}
--- src/test/resources/testcontainers/server.lua
+++ src/test/resources/testcontainers/server.lua
@@ -7,29 +7,29 @@ box.cfg {
     log_level = 6,
 }
 -- API user will be able to login with this password
-box.schema.user.create('api_user', { password = 'secret' })
+box.schema.user.create('api_user', { password = 'secret', if_not_exists = true })
+
 -- API user will be able to create spaces, add or remove data, execute functions
-box.schema.user.grant('api_user', 'read,write,execute', 'universe')
+box.schema.user.grant('api_user', 'read,write,execute', 'universe', nil, { if_not_exists = true })
 
+tester = box.schema.space.create('tester')
 
-s = box.schema.space.create('tester')
-
-s:format({
+tester:format({
     { name = 'id', type = 'unsigned' },
     { name = 'band_name', type = 'string' },
     { name = 'year', type = 'unsigned' }
 })
 
-s:create_index('primary', {
+tester:create_index('primary', {
     type = 'hash',
-    parts = {'id'}
+    parts = { 'id' }
 })
 
-s:insert{1, 'Roxette', 1986}
-s:insert{2, 'Scorpions', 2015}
-s:insert{3, 'Ace of Base', 1993}
+tester:insert { 1, 'Roxette', 1986 }
+tester:insert { 2, 'Scorpions', 2015 }
+tester:insert { 3, 'Ace of Base', 1993 }
 
-s:create_index('secondary', {
+tester:create_index('secondary', {
     type = 'hash',
-    parts = {'band_name'}
+    parts = { 'band_name' }
 })
