830f9d5f7fc7d9d3be0cbb8c60eaddcd3b8506c5
Andrei Punko <andd3dfx@gmail.com>
2024-08-24 22:59:36 +0300

    Add script to add properties on Win OS

---
+++ run-on-win-os-before-first-time-build.bat
@@ -0,0 +1,3 @@
+setx DOCKER_LOCATION "C:\\Program Files\\Docker\\Docker\\resources\\bin\\docker.exe"
+
+setx DOCKER_COMPOSE_LOCATION "C:\\Program Files\\Docker\\Docker\\resources\\bin\\docker-compose.exe"
